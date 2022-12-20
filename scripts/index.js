c000368c4304d58cf2699ac2bd2faced7f38529c
Bogdan Sutuzhko <julfy-bs@yandex.ru>
2022-12-20 16:21:17 +0300

    feat: добавляет бургер меню для телефонов.

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,21 @@
-const appearance = document.querySelector('.appearance');
-const modeButton = appearance.querySelector('.switch');
+const themeButtonArray = document.querySelectorAll('.switch');
 const page = document.querySelector('.page');
+const buttonOpenBurger = document.querySelector('.burger');
+const popupBurger = document.querySelector('.popup_role_burger');
+const buttonCloseBurger = popupBurger.querySelector('.popup__close');
 
 let userTheme = localStorage.getItem('theme') || '';
 let isUserPrefersDarkMode = false;
+
 const setTheme = (theme) => {
   localStorage.setItem('theme', theme);
   userTheme = theme;
   if (theme === 'dark') {
     page.classList.remove('page_theme_light');
-    modeButton.classList.add('switch_active');
+    themeButtonArray.forEach((item) => item.classList.add('switch_active'));
   } else {
     page.classList.remove('page_theme_dark');
-    modeButton.classList.remove('switch_active');
+    themeButtonArray.forEach((item) => item.classList.remove('switch_active'));
   }
   page.classList.add(`page_theme_${theme}`);
 };
@@ -52,6 +55,14 @@ document.addEventListener('DOMContentLoaded', () => {
   findAppearance();
 });
 
-modeButton.addEventListener('click', () => {
-  toggleTheme();
-});
+themeButtonArray.forEach((item) =>
+  item.addEventListener('click', () => {
+    toggleTheme();
+  })
+);
+
+const openPopup = (popup) => popup.classList.add('popup_active');
+const closePopup = (popup) => popup.classList.remove('popup_active');
+
+buttonOpenBurger.addEventListener('click', () => openPopup(popupBurger));
+buttonCloseBurger.addEventListener('click', () => closePopup(popupBurger));
