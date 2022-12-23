84e21581e285adb8860d9eefe1ebf4238b998e3f
Bogdan Sutuzhko <julfy-bs@yandex.ru>
2022-12-23 20:15:08 +0300

    feat: добавляет секцию surface, добавляет функционал слайдера.

--- scripts/index.js
+++ scripts/index.js
@@ -6,10 +6,16 @@ const buttonCloseBurger = popupBurger.querySelector('.popup__close');
 const subscriptionForm = document.querySelector('.subscription__form');
 const subscriptionInput = subscriptionForm.querySelector('.subscription__input');
 const subscriptionButton = subscriptionForm.querySelector('.subscription__button');
-
+const slider = document.querySelector('.section__wrapper_position_surface');
+const sliderContent = slider.querySelector('.surface__slider_role_content');
+const sliderImage = slider.querySelector('.surface__slider_role_image');
+const slidesContentArray = sliderContent.querySelectorAll('.surface__slider-item');
+const slidesImageArray = sliderImage.querySelectorAll('.surface__slider-item');
+const sliderBtnPrev = slider.querySelector('.surface__button_direction_prev');
+const sliderBtnNext = slider.querySelector('.surface__button_direction_next');
 let userTheme = localStorage.getItem('theme') || '';
 let isUserPrefersDarkMode = false;
-
+let activeSlide = 0;
 const setTheme = (theme) => {
   localStorage.setItem('theme', theme);
   userTheme = theme;
@@ -22,7 +28,6 @@ const setTheme = (theme) => {
   }
   page.classList.add(`page_theme_${theme}`);
 };
-
 const toggleTheme = () => {
   if (userTheme === 'light') {
     setTheme('dark');
@@ -30,7 +35,6 @@ const toggleTheme = () => {
     setTheme('light');
   }
 };
-
 const setPreferenceTheme = () => {
   isUserPrefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
   if (isUserPrefersDarkMode) {
@@ -41,11 +45,9 @@ const setPreferenceTheme = () => {
     setTheme('light');
   }
 };
-
 const setPresetTheme = (presetTheme) => {
   setTheme(presetTheme);
 };
-
 const findAppearance = () => {
   if (userTheme === '') {
     setPreferenceTheme();
@@ -55,14 +57,50 @@ const findAppearance = () => {
 };
 const openPopup = (popup) => popup.classList.add('popup_active');
 const closePopup = (popup) => popup.classList.remove('popup_active');
+const setSlideActive = (slide) => {
+  const activeClass = 'surface__slider-item_active';
+  slidesContentArray.forEach(item => item.classList.remove(activeClass));
+  slidesImageArray.forEach(item => item.classList.remove(activeClass));
+  slidesContentArray[slide].classList.add(activeClass);
+  slidesImageArray[slide].classList.add(activeClass);
+};
+const moveContentSlider = (slide) => {
+  return `translateX(calc(100% * ${slide} * -1)`;
+};
+const moveImageSlider = (slide) => {
+  return `translateX(calc((50% + 20px) * ${slide} * -1)`;
+};
+const openSlide = (slide) => {
+  setSlideActive(slide);
+  sliderContent.style.transform = moveContentSlider(slide);
+  sliderImage.style.transform = moveImageSlider(slide);
+};
+const slideNext = (amount, position) => {
+  if (position === amount - 1) {
+    activeSlide = 0;
+  }
+  if (position >= 0 && position < amount - 1) {
+    activeSlide++;
+  }
+  openSlide(activeSlide);
+};
+const slidePrev = (amount, position = 0) => {
+  if (position === 0) {
+    activeSlide = amount - 1;
+  }
+  if (position >= 1 && position < amount) {
+    activeSlide--;
+  }
+  openSlide(activeSlide);
+};
 document.addEventListener('DOMContentLoaded', () => {
   findAppearance();
 });
-themeButtonArray.forEach((item) =>
+themeButtonArray.forEach((item) => {
   item.addEventListener('click', () => {
     toggleTheme();
-  })
-);
+  });
+});
 buttonOpenBurger.addEventListener('click', () => openPopup(popupBurger));
 buttonCloseBurger.addEventListener('click', () => closePopup(popupBurger));
 subscriptionForm.addEventListener('submit', () => {
@@ -73,3 +111,5 @@ subscriptionForm.addEventListener('submit', () => {
     subscriptionInput.value = '';
   }, 5000);
 });
+sliderBtnPrev.addEventListener('click', () => slidePrev(slidesContentArray.length, activeSlide));
+sliderBtnNext.addEventListener('click', () => slideNext(slidesContentArray.length, activeSlide));
